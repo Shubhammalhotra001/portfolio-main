b34bd866960fda2c6867eda5ecedd913f83e4798
Shubhammalhotra001 <shubhammalhotra9728@gmail.com>
2026-01-16 00:42:11 +0530

    Projects updated

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -12,64 +12,7 @@ export function Project() {
     <Container id="project">
       <h2>My Projects</h2>
       <div className="projects">
-        {/* TileFusion */}
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-              <a
-                href="https://github.com/Shubhammalhotra001/TileFusion"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <svg
-                  width="40"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#23ce6b "
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <title>Code</title>
-                  <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-                </svg>
-              </a>
-              <div className="project-links">
-                <a
-                  href="https://tile-fusion-jet.vercel.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                  title="Demo"
-                >
-                  <img src={externalLink} alt="Visit site" />
-                </a>
-              </div>
-            </header>
-            <div className="project-image1">
-              <img src={TileFusion} alt="TileFusion Project" width="100%" height="220px" />
-            </div>
-            <div className="body">
-              <h3>TileFusion – 2048 Puzzle Game</h3>
-              <p>
-                A modern React-based 2048 game with a sleek dark interface, glowing gradient tiles, and smooth Framer Motion animations.
-                Designed with ⚡ Tailwind CSS, it features an interactive particle background, glassmorphism effects, and a fully responsive layout 
-                merge tiles to reach 2048 and win.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>React.js</li>
-                <li>Framer Motion</li>
-                <li>Tailwind CSS</li>
-                <li>JavaScript (ES6+)</li>
-                <li>Glassmorphism</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
+        
         {/* SmartSnip */}
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
